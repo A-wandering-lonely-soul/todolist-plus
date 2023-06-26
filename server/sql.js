1f6e2b547da5b5d63541a0c318176b3f0eda2307
iioouu <11335602+iioouu@user.noreply.gitee.com>
2023-06-26 07:37:37 +0000

    update server/sql.js.
    Signed-off-by: iioouu <11335602+iioouu@user.noreply.gitee.com>

--- server/sql.js
+++ server/sql.js
@@ -2,7 +2,7 @@ module.exports = {
 	CREATED_TASK_GROUP: "INSERT INTO TASK_GROUP (title, date) VALUES('${title}', '${date}')",
 	SELECT_TASK_GROUP:
 		"select L.id, l.title, l.description, l.enclosure, l.`level`, l.date, G.TITLE AS g_title, G.id as group_id from TASK_GROUP G left join  TASK_LIST L  on  G.id =L.group_id",
-	UPDATE_TASK_GROUP_TITLE: "UPDATE TASK_GROUP SET title=${title} WHERE id=${id}",
+	UPDATE_TASK_GROUP_TITLE: "UPDATE TASK_GROUP SET title='${title}' WHERE id=${id}",
 	CREATED_TASK_LIST:
 		"INSERT INTO TASK_LIST (title, description, enclosure, level, group_id, date) VALUES('${title}', '${description}', '${enclosure}', '${level}', '${group_id}', '${date}')",
 	UPDATE_TASK_LIST:
